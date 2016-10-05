b2a658addf31e3b927e45af1e773bbb6d0a83cec
Marcin Iwanicki <marcin.iwanicki@appliwings.com>
2016-10-05 19:43:55 +0100

    Reject publish request if some of the local changes have not been pushed to origin.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 GIT_STATUS_LINES=$(git status --porcelain | wc -l)
-GIT_REMOTE_LOCAL_DIFF=$(git log master..master | wc -l)
+GIT_REMOTE_LOCAL_DIFF=$(git log origin/master..master | wc -l)
 
 if [ $GIT_STATUS_LINES -ge 1 ] && [ $GIT_REMOTE_LOCAL_DIFF -ne 0 ] ; then
-	echo "Error: Cannot publish changes. Some of the files are not committed. See git status to get more details.";
+	echo "Error: Cannot publish changes. Some changes have not been committed or pushed. See git status to get more details.";
  	git status;
 	exit 1;
 fi
